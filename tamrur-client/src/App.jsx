474529af5bcbef6ff609fb101e67a10c523b9c37
TuvalZit18 <18tuvalzit18@gmail.com>
2026-08-11 23:02:53 +0300

    feat: implement create event form with location picker and event creation logic
    feat: implement create event form with location picker and event creation logic

--- tamrur-client/src/App.jsx
+++ tamrur-client/src/App.jsx
@@ -2,15 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/auth/LoginPage";
 import SignupPage from "./pages/auth/SignupPage";
 import DashboardPage from "./pages/guest/DashboardPage";
-
+import CreateEventForm from "./components/events/CreateEventForm";
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="*" element={<LoginPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/create-event" element={<CreateEventForm />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="*" element={<LoginPage />} />
+      </Routes>
+    </>
   );
 };
 
