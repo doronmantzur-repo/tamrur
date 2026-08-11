9d84e82f7bc4c489dc31c8894655f8519a92706d
TuvalZit18 <18tuvalzit18@gmail.com>
2026-08-11 23:09:32 +0300

    feat: add route for create event form
    feat: add route for create event form

--- tamrur-client/src/App.jsx
+++ tamrur-client/src/App.jsx
@@ -3,12 +3,13 @@ import LoginPage from "./pages/auth/LoginPage";
 import SignupPage from "./pages/auth/SignupPage";
 import DashboardPage from "./pages/guest/DashboardPage";
 import AerialEvacuationPage from "./pages/airforce/AerialEvacuationPage";
-
+import CreateEventForm from "./components/events/CreateEventForm";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/create-event" element={<CreateEventForm />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/airforce" element={<AerialEvacuationPage />} />
       <Route path="*" element={<LoginPage />} />
