02a0558f69afd4f649fc6205dbfe598eb8ae6c5d
doronm <doronm@chain-reaction.io>
2026-08-11 16:58:55 +0300

    feat: add dashboard page component and update routing

--- tamrur-client/src/App.jsx
+++ tamrur-client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/auth/LoginPage";
 import SignupPage from "./pages/auth/SignupPage";
+import DashboardPage from "./pages/guest/DashboardPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="*" element={<LoginPage />} />
     </Routes>
   );
